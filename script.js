dfc158b618e92259ee07b4eacb029b218e15010c
achieveoneparrk <park_achieveone@naver.com>
2025-11-19 21:41:19 +0900

    다시 절대경로로...

--- script.js
+++ script.js
@@ -594,7 +594,7 @@ function shareKakao() {
     content: {
       title: '박성일 ❤️ 김채린 결혼합니다',
       description: '2026년 02월 08일 일요일 오후 12시 20분\n웨딩 시그니처 2층 트리니티홀\nhttps://somchae.wedding',
-      imageUrl: 'https://achieveonepark.github.io/wedding-invitation/images/kakao.JPG',
+      imageUrl: 'https://somchae.wedding/images/kakao.JPG',
       link: {
         mobileWebUrl: 'https://somchae.wedding',
         webUrl: 'https://somchae.wedding',
